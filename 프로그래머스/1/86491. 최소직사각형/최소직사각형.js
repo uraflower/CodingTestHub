60e33387e8c80dc887060a5076f5e4f13781baa1
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-25 01:41:20 +0900

    [level 1] Title: 최소직사각형, Time: 8.69 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -2,13 +2,11 @@ function solution(sizes) {
     const sortedSizes = sizes.map(
         size => size.sort((w, h) => Number(w) - Number(h)));
     
-    const widthList = [];
-    const heightList = [];
-    
-    sortedSizes.forEach(([w, h]) => {
-        widthList.push(w);
-        heightList.push(h);
-    });
+    const [widthList, heightList] = sortedSizes.reduce(([wAcc, hAcc], [w, h]) => {
+        wAcc.push(w);
+        hAcc.push(h);
+        return [wAcc, hAcc];
+    }, [[], []]);
     
     const w = Math.max(...widthList);
     const h = Math.max(...heightList);
