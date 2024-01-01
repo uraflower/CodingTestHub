b90e43fe9b9c878df591a92c0e2e2fcc4a030b97
Finn <82873315+uraflower@users.noreply.github.com>
2024-01-01 18:31:01 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 144 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.js
@@ -0,0 +1,22 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.on('line', function (line) {
+  readline.close();
+  solution(Number(line));
+});
+
+function solution(num) {
+  const stars = [];
+  for (let i = 1; i <= num; i++) {
+    const whitespace = ' '.repeat(num - i);
+    const star = '*'.repeat(i * 2 - 1);
+    stars.push(whitespace + star);
+  }
+
+  console.log(stars.join('\n'));
+  stars.pop();
+  console.log(stars.reverse().join('\n'));
+}
