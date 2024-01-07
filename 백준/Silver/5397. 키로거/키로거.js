873a1d1ccc36eac4bb9cdc40da8f45adce048273
Finn <82873315+uraflower@users.noreply.github.com>
2024-01-07 15:22:09 +0900

    [Silver II] Title: 키로거, Time: 1212 ms, Memory: 279584 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.js
@@ -0,0 +1,82 @@
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+class ListNode {
+  constructor(data, prev = null, next = null) {
+    this.data = data;
+    this.prev = prev;
+    this.next = next;
+  }
+}
+
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  const [T, ...lines] = input;
+  solution(Number(T), lines);
+});
+
+function solution(T, lines) {
+  lines.forEach((string) => {
+    const head = new ListNode('');
+    let pointer = head;
+
+    Array.from(string).forEach((key) => {
+      // console.log(pointer);
+
+      if (key === '<') pointer = movePointerToLeft(pointer);
+      else if (key === '>') pointer = movePointerToRight(pointer);
+      else if (key === '-') pointer = deletePrev(pointer);
+      else pointer = insertKey(pointer, key);
+    });
+
+    // head부터 순회하며 출력
+    printPassword(head);
+  });
+}
+
+function movePointerToLeft(pointer) {
+  return pointer.prev ?? pointer;
+}
+
+function movePointerToRight(pointer) {
+  return pointer.next ?? pointer;
+}
+
+function deletePrev(pointer) {
+  // 포인터가 가리키는 노드를 삭제
+  if (!pointer.prev) return pointer; // pointer === head이면 생략
+
+  const prevNode = pointer.prev;
+  const nextNode = pointer.next;
+
+  if (prevNode) prevNode.next = nextNode;
+  if (nextNode) nextNode.prev = prevNode;
+
+  return prevNode;
+}
+
+function insertKey(pointer, key) {
+  // 포인터가 가리키는 노드의 다음 위치에 새 노드를 삽입
+  const nextNode = pointer.next; // nullable
+  const newNode = new ListNode(key, pointer, nextNode);
+
+  if (nextNode) nextNode.prev = newNode;
+  pointer.next = newNode;
+
+  return newNode;
+}
+
+function printPassword(head) {
+  const password = [];
+  let pointer = head.next;
+  while (pointer) {
+    password.push(pointer.data);
+    pointer = pointer.next;
+  }
+  console.log(password.join(''));
+}
