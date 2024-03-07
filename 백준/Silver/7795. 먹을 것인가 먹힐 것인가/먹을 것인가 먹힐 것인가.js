fb29870e1ca3083aebe2a0783ea30239dfe5df10
Finn <82873315+uraflower@users.noreply.github.com>
2024-03-08 00:20:03 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 1204 ms, Memory: 21548 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.js
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.js
@@ -9,14 +9,11 @@ const [T, ...testCases] = fs
 for (let t = 0; t < T; t++) {
   const As = testCases[t * 3 + 1];
   const Bs = testCases[t * 3 + 2].sort((a, b) => Number(a) - Number(b));
-  const memo = {};
 
   const sum = As.reduce((sum, A) => {
-    if (memo[A] === undefined) {
-      const count = Bs.findIndex((B) => A <= B);
-      memo[A] = count === -1 ? Bs.length : count;
-    }
-    return sum + memo[A];
+    let count = Bs.findIndex((B) => A <= B);
+    if (count === -1) count = Bs.length;
+    return sum + count;
   }, 0);
 
   console.log(sum);
