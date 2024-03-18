9f3aabf02a5c4114a984a920fa8acede7de326b7
Finn <82873315+uraflower@users.noreply.github.com>
2024-03-19 01:26:32 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 9440 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,40 +1,35 @@
 const fs = require('fs');
-const [V, E, ...edges] = fs
+let [V, E, ...edges] = fs
   .readFileSync('/dev/stdin')
   .toString()
   .trim()
   .split('\n');
+V = Number(V);
+E = Number(E);
 
-const adjArr = Array.from({ length: V + 1 }).map(() => []);
-const visited = new Set();
-solution();
+// 인접 리스트 구현
+const adjList = Array.from({ length: V + 1 }).map(() => []);
+edges.forEach((line) => {
+  const [start, end] = line.split(' ').map(Number);
+  adjList[start].push(end);
+  adjList[end].push(start);
+});
 
-function solution() {
-  initialize();
-  bfs();
-  console.log(visited.size - 1);
+// 1번 컴퓨터부터 탐색(bfs)
+let cnt = 0;
+const queue = [1];
+const visited = new Set(queue);
+
+while (queue.length > 0) {
+  const v = queue.shift();
+
+  adjList[v].forEach((neighbor) => {
+    if (!visited.has(neighbor)) {
+      visited.add(neighbor);
+      queue.push(neighbor);
+      cnt += 1;
+    }
+  });
 }
 
-function initialize() {
-  for (const edge of edges) {
-    const [start, end] = edge.split(' ').map(Number);
-    adjArr[start].push(end);
-    adjArr[end].push(start);
-  }
-}
-
-function bfs() {
-  const queue = [1];
-
-  while (queue.length > 0) {
-    const cur = queue.shift();
-    visited.add(cur);
-
-    adjArr[cur].forEach((next) => {
-      if (!visited.has(next)) {
-        queue.push(next);
-        visited.add(next);
-      }
-    });
-  }
-}
+console.log(cnt);
