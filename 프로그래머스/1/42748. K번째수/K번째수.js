20ac93e9c38c59616998e53d4862a07a16a673b8
Finn <82873315+uraflower@users.noreply.github.com>
2025-01-04 14:05:57 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,7 +1,6 @@
 function solution(array, commands) {
-    const answer = commands.map(([i, j, k]) => {
-       const sorted = array.slice(i - 1, j).sort((a, b) => Number(a) - Number(b));
-       return sorted[k - 1];
+    return commands.map((command) => {
+        const [i, j, k] = command;
+        return array.slice(i-1, j).sort((a,b)=>Number(a)-Number(b))[k-1];
     });
-    return answer;
 }
