07e8c2612daf842bc44e4ad9e626be4a86ed7ee7
Finn <82873315+uraflower@users.noreply.github.com>
2025-01-14 14:51:56 +0900

    [level 2] Title: 소수 찾기, Time: 91.25 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,23 +1,27 @@
 function solution(numbers) {
-    const created = new Set();
-    search(created, '', [...numbers]);
-    console.log(created);
-    return Array.from(created).filter((number) => isPrime(number)).length;
+    const primeNumbers = new Set();
+    const cards = [...numbers].map((number) => ({ number, isSelected: false }));
+    
+    function recurse(numbers, currentNumber) {
+        numbers.forEach(({ number, isSelected }, idx) => {
+            if (!isSelected) {
+                numbers[idx].isSelected = true;
+                const current = Number(currentNumber + number);
+                if (isPrimeNumber(current)) primeNumbers.add(current);
+                recurse(numbers, currentNumber + number);
+                numbers[idx].isSelected = false;
+            }
+        });
+    }
+    
+    recurse(cards, '');
+    
+    return primeNumbers.size;
 }
 
-function search(created, fixed, others) {
-    others.forEach((current, i) => {
-        created.add(Number(fixed + current));
-        
-        const exceptedCurrent = [...others];
-        exceptedCurrent.splice(i, 1);
-        
-        search(created, fixed + current, exceptedCurrent);
-    })
-}
-
-function isPrime(number) {
+function isPrimeNumber(number) {
     if (number === 0 || number === 1) return false;
+    
     for (let i = 2; i <= number / 2; i++) {
         if (number % i === 0) return false;
     }
