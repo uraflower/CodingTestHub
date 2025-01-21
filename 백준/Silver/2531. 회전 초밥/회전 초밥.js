75e940ada3cc19d08414d32fc83dde5bfe0830b2
Finn <82873315+uraflower@users.noreply.github.com>
2025-01-21 15:19:57 +0900

    [Silver I] Title: 회전 초밥, Time: 208 ms, Memory: 16212 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.js
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.js
@@ -8,39 +8,30 @@ const inputs = fs
 const [N, d, k, couponSushi] = inputs.shift().split(' ').map(Number);
 const sushes = inputs.map(Number);
 
-// k개만큼 슬라이스해서 Set에 넣고 size를 잼
-// max보다 size가 크면 max 갱신
-// 이 때 Set이 쿠폰 번호와 같은 스시를 가지고 있지 않으면 max + 1
-// N번 반복
-
-let start = 0;
-let end = k - 1;
-let max = 0; // answer
 const counts = Array.from({ length: d + 1 }).fill(0);
+counts[couponSushi]++; // 쿠폰 스시 미리 먹기
+let cnt = 1; // 먹은 스시의 종류 수
+let max = 0; // cnt 중 가장 큰 값. answer
 
-// counts 초기화. 처음부터 k-1번째 스시까지를 순회하며 counts 업데이트
-for (let i = start; i < end; i++) {
+// counts 초기화
+for (let i = 0; i < k; i++) {
+  if (counts[sushes[i]] === 0) cnt++;
   counts[sushes[i]]++;
 }
 
-while (start < N) {
-  counts[sushes[end]]++;
+max = cnt;
 
-  const selected = new Set(
-    counts.reduce((selected, count, idx) => {
-      if (count > 0) selected.push(idx);
-      return selected;
-    }, []),
-  );
+for (let start = 0; start < N; start++) {
+  counts[[sushes[start]]]--;
+  if (counts[sushes[start]] === 0) cnt--;
 
-  if (max <= selected.size) {
-    max = selected.size;
-    if (!selected.has(couponSushi)) max = max + 1;
-  }
+  const end = (start + k) % N;
 
-  counts[sushes[start]]--;
-  start = start + 1;
-  end = (end + 1) % N;
+  if (counts[sushes[end]] === 0) cnt++;
+  counts[[sushes[end]]]++;
+
+  // max 업데이트
+  if (max < cnt) max = cnt;
 }
 
 console.log(max);
