6eebbfa6529c91dbb5f94fb4d5d29d8b143f5f8d
Finn <82873315+uraflower@users.noreply.github.com>
2025-02-19 10:18:55 +0900

    [level 2] Title: 타겟 넘버, Time: 10.15 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,18 +1,22 @@
-let cnt = 0;
-
 function solution(numbers, target) {
-    search(0, numbers, target, 0);
-    return cnt;
-}
-
-function search(calculated, remains, target, position) {
-    if (position >= remains.length) {
-        if (calculated === target) cnt++;
-        return;
+    let count = 0;
+    
+    function recurse(idx, sum) {
+        const added = sum + numbers[idx];
+        const subtracted = sum - numbers[idx];
+        
+        if (idx + 1 === numbers.length) {
+            if (added === target) count++;
+            if (subtracted === target) count++;
+            
+            return;
+        }
+        
+        recurse(idx + 1, added);
+        recurse(idx + 1, subtracted);
     }
     
-    const num = remains[position];
-    
-    search(calculated + num, remains, target, position+1);
-    search(calculated - num, remains, target, position+1);
+    recurse(0, 0);
+    return count;
 }
+
