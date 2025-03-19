02bcfc84bfb28d66a018ff70a48e3c283c54a694
Finn <82873315+uraflower@users.noreply.github.com>
2025-03-19 12:16:50 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,8 +1,17 @@
 function solution(n, arr1, arr2) {
-    const answer = arr1.map((number, i) => {
-        let binary = (number | arr2[i]).toString(2);
-        binary = '0'.repeat(n - binary.length) + binary;
-        return binary.replaceAll('0', ' ').replaceAll('1', '#');
-    })
+    const answer = [];
+    
+    for (let i = 0; i < n; i++) {
+        let bin1 = arr1[i].toString(2);
+        let bin2 = arr2[i].toString(2);
+        
+        bin1 = '0'.repeat(n - bin1.length) + bin1;
+        bin2 = '0'.repeat(n - bin2.length) + bin2;
+        
+        const result = Array.from(bin1).map((v, i) => v === '1' || bin2[i] === '1' ? '#' : ' ').join('');
+        
+        answer.push(result);
+    }
+    
     return answer;
 }
