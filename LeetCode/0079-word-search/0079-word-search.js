643c1b58e954a871c29df41ff8bbd331d4c31426
Finn <82873315+uraflower@users.noreply.github.com>
2025-04-25 00:46:53 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/0079-word-search/0079-word-search.js
@@ -0,0 +1,39 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+const exist = function (board, word) {
+    const row = board.length;
+    const col = board[0].length;
+
+    const dr = [0, 0, 1, -1];
+    const dc = [1, -1, 0, 0];
+    const visited = Array.from({ length: row }).map(() => Array.from({ length: col }));
+    
+    function dfs(r, c, charIdx) {        
+        if (charIdx === word.length) return true;
+        if (!(0 <= r && r < row && 0 <= c && c < col)) return false;
+        if (board[r][c] !== word[charIdx]) return false;
+        if (visited[r][c]) return false;
+
+        visited[r][c] = true;
+        console.log(r, c, board[r][c], charIdx);
+        for (let i = 0; i < 4; i++) {
+            const nr = r + dr[i];
+            const nc = c + dc[i];
+            if (dfs(nr, nc, charIdx + 1)) return true;
+        }
+        visited[r][c] = false;
+        return false;
+    }
+    
+
+    for (let r = 0; r < row; r++) {
+        for (let c = 0; c < col; c++) {
+            if (dfs(r, c, 0)) return true;
+        }
+    }
+
+    return false;
+};
