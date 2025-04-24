573bcc8b7ca116d09a99a37cc3a67562d42372b2
Finn <82873315+uraflower@users.noreply.github.com>
2025-04-24 17:40:23 +0900

    Time: 0 ms (100%), Space: 53.7 MB (54.88%) - LeetHub

--- LeetCode/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ LeetCode/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -1,7 +1,19 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
+
+
 const findMin = function(nums) {
-    return Math.min(...nums);
-};
+    let left = 0;
+    let right = nums.length - 1;
+    let mid;
+
+    while (left < right) {
+        mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] < nums[right]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return nums[left];
+}
