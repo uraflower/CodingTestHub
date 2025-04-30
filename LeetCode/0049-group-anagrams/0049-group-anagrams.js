d986953d06929a5a677dcbc04858713d89a91f98
Finn <82873315+uraflower@users.noreply.github.com>
2025-04-30 11:50:32 +0900

    Time: 39 ms (35.37%), Space: 68.5 MB (37.63%) - LeetHub

--- LeetCode/0049-group-anagrams/0049-group-anagrams.js
+++ LeetCode/0049-group-anagrams/0049-group-anagrams.js
@@ -4,41 +4,13 @@
  * @return {string[][]}
  */
 const groupAnagrams = function(strs) {
-    const groups = new Map();
+    const groups = {};
 
     strs.forEach((str) => {
-        let pushed = false;
-        const counterString = countChar(str);
-
-        for (const key of groups.keys()) {
-            if (key.length === counterString.length && compare(key, counterString)) {
-                groups.set(key, [...groups.get(key), str]);
-                pushed = true;
-            }
-        }
-
-        if (!pushed) {
-            groups.set(counterString, [str]);
-        }
+        const key = [...str].sort().join('');
+        if (!groups[key]) groups[key] = [];
+        groups[key].push(str);
     });
 
-    const answer = [];
-    for (const value of groups.values()) {
-        answer.push(value);
-    }
-    return answer;
+    return  Object.values(groups)
 };
-
-function countChar(str) {
-    const counter = {};
-    for (let char of str) {
-        counter[char] = counter[char] + 1 || 1;
-    }
-    counter.length = str.length;
-    return counter;
-}
-
-function compare(a, b) {
-    return Object.keys(a).length === Object.keys(b).length
-        && Object.entries(a).every(([keyA, valueA]) => b[keyA] === valueA);
-}
