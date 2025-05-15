c62ce1d03854d5d4ebeb3ff1312393366a0ad2f5
Finn <82873315+uraflower@users.noreply.github.com>
2025-05-15 14:51:30 +0900

    Time: 4 ms (13.14%), Space: 55.3 MB (19.11%) - LeetHub

--- LeetCode/0062-unique-paths/0062-unique-paths.js
+++ LeetCode/0062-unique-paths/0062-unique-paths.js
@@ -4,26 +4,29 @@
  * @return {number}
  */
 const uniquePaths = function (m, n) {
+    const grid = Array.from({length: m}, () => Array(n).fill(0));
     let r = 0;
     let c = 0;
     const queue = [[r, c]];
-    let paths = 0;
+    grid[r][c] = 1;
 
     while (queue.length) {
         const [x, y] = queue.shift();
 
         if (x === m - 1 && y === n - 1) {
-            paths += 1;
             continue;
         }
 
         if (0 <= x + 1 && x + 1 < m) {
-            queue.push([x + 1, y]);
+            if (grid[x+1][y] === 0) queue.push([x + 1, y]);
+            grid[x+1][y] += grid[x][y];
         }
+
         if (0 <= y + 1 && y + 1 < n) {
-            queue.push([x, y + 1]);
+            if (grid[x][y+1] === 0) queue.push([x, y + 1]);
+            grid[x][y+1] += grid[x][y];
         }
     }
 
-    return paths;
+    return grid[m-1][n-1];
 };
