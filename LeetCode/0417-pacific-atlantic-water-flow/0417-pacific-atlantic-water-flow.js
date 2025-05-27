70e0009822c4a4eab33f2c732516f2b1151429f5
Finn <82873315+uraflower@users.noreply.github.com>
2025-05-27 15:40:21 +0900

    Time: 25 ms (69.18%), Space: 66 MB (57.16%) - LeetHub

--- LeetCode/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.js
+++ LeetCode/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.js
@@ -3,55 +3,46 @@
  * @return {number[][]}
  */
 const pacificAtlantic = function (heights) {
-    // 각 cell을 순회
-    // cell의 동서남북 살피면서 물이 흐를 수 있으면 search
-    // search 결과 태평양 및 대서양에 닿으면 result에 추가
-
     const rows = heights.length;
     const cols = heights[0].length;
 
-    // 각 cell에 태평양 및 대서양에 도달 가능한지 정보를 저장(메모)
-    const info = Array.from({ length: rows }, () => Array.from({ length: cols }, () => ({ p: false, a: false, visited: false })));
+    const pacific = Array.from({ length: rows }, () => Array(cols).fill(false));
+    const atlantic = Array.from({ length: rows }, () => Array(cols).fill(false));
+
+    const direction = [[0, 1], [0, -1], [1, 0], [-1, 0]];
 
     // search
-    function dfs(r, c) {
-        info[r][c].visited = true;
+    function bfs(r, c, visited) {
+        visited[r][c] = true;
 
-        if (r === 0 || c === 0) {
-            info[r][c].p = true;
-        }
-        if (r === rows - 1 || c === cols - 1) {
-            info[r][c].a = true;
-        }
+        const queue = [[r, c]];
 
-        const dx = [0, 0, 1, -1];
-        const dy = [1, -1, 0, 0];
+        while (queue.length) {
+            const [r, c] = queue.shift();
 
-        for (let i = 0; i < 4; i++) {
-            if (info[r][c].p && info[r][c].a) {
-                return { p: true, a: true };
-            }
+            for (const [dr, dc] of direction) {
+                const nr = r + dr;
+                const nc = c + dc;
 
-            const nr = r + dx[i];
-            const nc = c + dy[i];
-
-            if (0 <= nr && nr < heights.length && 0 <= nc && nc < heights[0].length && heights[r][c] >= heights[nr][nc]) {
-                let p, a;
-                if (info[nr][nc].visited) {
-                    ({ p, a } = info[nr][nc]);
-                } else {
-                    ({ p, a } = dfs(nr, nc));
+                if (0 <= nr && nr < rows && 0 <= nc && nc < cols
+                    && !visited[nr][nc]
+                    && heights[nr][nc] >= heights[r][c]
+                ) {
+                    queue.push([nr, nc]);
+                    visited[nr][nc] = true;
                 }
-
-                info[r][c].p = info[r][c].p || p;
-                info[r][c].a = info[r][c].a || a;
             }
         }
+    }
 
-        return {
-            p: info[r][c].p,
-            a: info[r][c].a
-        }
+    for (let r = 0; r < rows; r++) {
+        bfs(r, 0, pacific);
+        bfs(r, cols - 1, atlantic);
+    }
+
+    for (let c = 0; c < cols; c++) {
+        bfs(0, c, pacific);
+        bfs(rows - 1, c, atlantic);
     }
 
     // 각 cell 순회 및 결과 계산
@@ -59,8 +50,7 @@ const pacificAtlantic = function (heights) {
 
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
-            const { p, a } = dfs(r, c);
-            if (p && a) {
+            if (pacific[r][c] && atlantic[r][c]) {
                 result.push([r, c]);
             }
         }
