40f86643192cdcf7dacfa349dd237a813f57303b
Finn <82873315+uraflower@users.noreply.github.com>
2025-06-12 17:33:21 +0900

    Time: 13 ms (29.36%), Space: 67.2 MB (11.73%) - LeetHub

--- LeetCode/0056-merge-intervals/0056-merge-intervals.js
+++ LeetCode/0056-merge-intervals/0056-merge-intervals.js
@@ -6,16 +6,13 @@ const merge = function (intervals) {
     intervals.sort((a, b) => Number(a[0]) - Number(b[0]));
     const result = [];
 
-    for (const [s1, e1] of intervals) {
-        if (result.length > 0) {
-            const [s2, e2] = result[result.length - 1];
-            if (s1 <= e2) {
-                result[result.length - 1] = [Math.min(s1, s2), Math.max(e1, e2)];
-            } else {
-                result.push([s1, e1]);
-            }
+    for (const current of intervals) {
+        const last = result[result.length - 1];
+
+        if (last && current[0] <= last[1]) {
+            result[result.length - 1] = [Math.min(current[0], last[0]), Math.max(current[1], last[1])];
         } else {
-            result.push([s1, e1]);
+            result.push(current);
         }
     }
 
