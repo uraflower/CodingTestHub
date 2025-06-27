1544b471c6efb6069875f3954b2d6147c91cf737
Finn <82873315+uraflower@users.noreply.github.com>
2025-06-27 17:55:48 +0900

    Time: 2 ms (82.45%), Space: 59.2 MB (29.43%) - LeetHub

---
+++ LeetCode/0057-insert-interval/0057-insert-interval.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+const insert = function(intervals, newInterval) {
+    const merged = [];
+    let i = 0;
+
+    // 겹치지 않는 앞부분 push
+    while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+        merged.push(intervals[i]);
+        i++;
+    }
+
+    // 겹치는 부분 처리
+    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+    merged.push(newInterval);
+
+    // 겹치지 않는 뒷부분 push
+    while (i < intervals.length) {
+        merged.push(intervals[i]);
+        i++;
+    }
+
+    return merged;
+};
