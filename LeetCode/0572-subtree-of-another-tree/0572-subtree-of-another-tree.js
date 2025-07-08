28a57f5506401111fb39a7e5ee4881c7a70d6dba
Finn <82873315+uraflower@users.noreply.github.com>
2025-07-08 18:44:59 +0900

    Time: 11 ms (12.15%), Space: 62.8 MB (24.06%) - LeetHub

--- LeetCode/0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
+++ LeetCode/0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
@@ -11,38 +11,20 @@
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
-const isSubtree = function(root, subRoot) {
+const isSubtree = function (root, subRoot) {
     // 탐색을 하다가 서브트리의 루트 val과 일치하는 노드가 있으면
     // 거기서부터 비교 시작
     // 만약 비교했는데 다르면 비교 시작한 부분부터 다시
     // 아니면 두 트리를 정렬한 다음 비교 => 이건 val이 같은 경우가 많을 때가 있으니까 안될듯
 
-    const isSame = function(node1, node2) {
+    const isSame = function (node1, node2) {
         if (!node1 && !node2) return true;
-        
-        // if ((node1 && !node2) || (!node1 && node2)) return false;
-
         if (node1?.val !== node2?.val) return false;
 
         return isSame(node1.left, node2.left) && isSame(node1.right, node2.right);
     }
 
-    const queue = [root];
-
-    while (queue.length) {
-        const node = queue.shift();
-
-        if (node.left) {
-            queue.push(node.left);
-        }
-
-        if (node.right) {
-            queue.push(node.right);
-        }
-
-        if (isSame(node, subRoot)) {
-            return true;
-        }
-    }
-    return false;
+    if (!subRoot) return true;
+    if (!root) return false;
+    return isSame(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
 };
