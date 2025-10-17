65cd90afecec80e97fbf41cc79fe068fecc1d2fc
Finn <uraflower@ajou.ac.kr>
2025-10-17 15:06:10 +0900

    Time: 4 ms (53.86%), Space: 57.8 MB (45.13%) - LeetHub

--- LeetCode/0169-majority-element/0169-majority-element.js
+++ LeetCode/0169-majority-element/0169-majority-element.js
@@ -4,10 +4,16 @@
  */
 var majorityElement = function (nums) {
     const counter = {};
-
     nums.forEach((num) => counter[num] = counter[num] + 1 || 1);
 
-    const sortedEntries = Object.entries(counter).sort((a, b) => Number(b[1]) - Number(a[1]));
+    let maxK = null;
+    let maxV = 0;
+    for (const [k, v] of Object.entries(counter)) {
+        if (v > maxV) {
+            maxK = k;
+            maxV = v;
+        }
+    }
 
-    return Number(sortedEntries[0][0]);
+    return Number(maxK);
 };
