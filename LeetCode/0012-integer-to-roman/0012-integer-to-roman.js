fe50252ce2a72c63cb63a7c245081fd8b7ebe086
Finn <uraflower@ajou.ac.kr>
2025-10-21 17:22:33 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    let answer = '';
+    let current = num.toString();
+    let i = current.length - 1;
+
+    const symbols = { 
+        1000: 'M',
+        1000: 'M',
+        1000: 'M',
+        1000: 'M',
+    }
+
+    while (i-- >= 0) {
+        current[i]
+    }
+
+    return answer;
+};
